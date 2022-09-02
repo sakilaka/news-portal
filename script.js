6272e8567c8c5d047a73b54ad09d043f1c12b8aa
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-09-03 02:40:35 +0600

    displayCategory done

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const displayCategoryDetails = news =>{
     const catagoryDetail = document.getElementById('category-details');
     news.forEach(list =>{
         const listDiv = document.createElement('div');
-        listDiv.classList.add('row' , 'my-3');
+        listDiv.classList.add('row' , 'my-3','border' ,'border-dark');
         listDiv.innerHTML = `
         <div class="col-lg-2 col-sm-1 border-3" >
           <img src="${list.thumbnail_url}" class="rounded-start" alt="...">
@@ -53,8 +53,23 @@ const displayCategoryDetails = news =>{
         <div class="col-lg-10 col-sm-2">
             <div class="card-body ms-5">
               <h5 class="card-title fw-bold fs-3">${list.title}</h5>
-              <p class="card-text fs-4">${list.details.slice(0,300)}</p>
+              <p class="card-text fs-5">${list.details.slice(0,500)}</p>
+              <div class="d-flex">
+               <img class="rounded-pill" style="width:60px; height:60px;" src="${list.author.img}" >
+                 <div class="ms-2">
+                 <h5 >${list.author.name}</h5>
+                 <p>${list.author.published_date}</p>
+                 </div>
+
+                 <div class="d-flex" style="margin-left: 120px;margin-top: 8px;">
+                    <i class="fa-solid fa-eye mt-1"></i>
+                    <h5 class="ms-3">${list.total_view}</h5>
+                 </div>
+
+
+
               </div>
+            </div>
         </div>
         `
         catagoryDetail.appendChild(listDiv);
